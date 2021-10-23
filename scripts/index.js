1c1d4c01f25fc7d7ee4d8bdaaaeb3846966074e7
Marta <marti.arslanova@gmail.com>
2021-10-23 22:43:42 +0300

    Добавление единых функций для открытия и закрытия текущих попапов в проекте

--- scripts/index.js
+++ scripts/index.js
@@ -6,7 +6,8 @@ const profileDescr = profileContainer.querySelector('.profile__description');
 const locationPopupOpenBtn = profileContainer.querySelector('.profile__button');
 
 const profileEditPopup = document.querySelector('.popup_type_edit');
-const profileEditPopupCloseBtn = profileEditPopup.querySelector('.popup__close');
+const profileEditPopupCloseBtn =
+  profileEditPopup.querySelector('.popup__close');
 const profileEditForm = profileEditPopup.querySelector('.form');
 const userNameInput = profileEditPopup.querySelector('#user-name');
 const userDescrInput = profileEditPopup.querySelector('#user-descr');
@@ -22,7 +23,6 @@ const imagePopupCloseBtn = imagePopup.querySelector('.popup__close');
 const imageLeadElement = imagePopup.querySelector('.lead__img');
 const figcaptionLeadElement = imagePopup.querySelector('.lead__figcaption');
 const cardTemplate = document.querySelector('#card').content;
-
 
 // Данные
 const initialCards = [
@@ -53,14 +53,17 @@ const initialCards = [
 ];
 
 // Функции
-function openProfileEditPopup() {
-  profileEditPopup.classList.add('popup_opened');
+function openEditProfile() {
   userNameInput.value = profileName.innerText;
   userDescrInput.value = profileDescr.innerText;
+  openPopup(profileEditPopup);
 }
 
-function profileEditPopupClose() {
-  profileEditPopup.classList.remove('popup_opened');
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+}
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
 }
 
 function profileEditFormSubmit(evt) {
@@ -68,14 +71,6 @@ function profileEditFormSubmit(evt) {
   profileName.innerText = userNameInput.value;
   profileDescr.innerText = userDescrInput.value;
   profileEditPopupClose();
-}
-
-function openCardAddPopup() {
-  cardAddPopup.classList.add('popup_opened');
-}
-
-function cardAddPopupClose() {
-  cardAddPopup.classList.remove('popup_opened');
 }
 
 function createCardElement(link, title) {
@@ -86,23 +81,23 @@ function createCardElement(link, title) {
   return cardElement;
 }
 
-function addImageListener (cardElement, link, title) {
+function addImageListener(cardElement, link, title) {
   const cardImage = cardElement.querySelector('.card__img');
   cardImage.addEventListener('click', function () {
     imageLeadElement.src = link;
     figcaptionLeadElement.textContent = title;
-    imagePopup.classList.toggle('popup_opened');
+    openPopup(imagePopup);
   });
 }
 
-function addLikeBtnListener (cardElement) {
+function addLikeBtnListener(cardElement) {
   const cardLikeBtn = cardElement.querySelector('.card__like');
   cardLikeBtn.addEventListener('click', function (event) {
     event.target.classList.toggle('card__like_active');
   });
 }
 
-function addDeleteBtnListener (cardElement) {
+function addDeleteBtnListener(cardElement) {
   const cardDeleteBtn = cardElement.querySelector('.card__delete');
   cardDeleteBtn.addEventListener('click', function () {
     cardElement.remove();
@@ -130,31 +125,32 @@ function cardAdd(evt) {
   addLikeBtnListener(cardElement);
   addDeleteBtnListener(cardElement);
   cardsContainer.prepend(cardElement);
-  cardAddPopupClose();
-  locationNameInput.value = "";
-  locationImgLinkInput.value = "";
+  closePopup(cardAddPopup);
+  locationNameInput.value = '';
+  locationImgLinkInput.value = '';
 }
 
-function closeImagePopup () {
-  imagePopup.classList.remove('popup_opened');
-};
 // Обработчики
-profileEditBtn.addEventListener('click', openProfileEditPopup);
+profileEditBtn.addEventListener('click', openEditProfile);
 
-profileEditPopupCloseBtn.addEventListener('click', profileEditPopupClose);
+profileEditPopupCloseBtn.addEventListener('click', function () {
+  closePopup(profileEditPopup);
+});
 
 profileEditForm.addEventListener('submit', profileEditFormSubmit);
 
-locationPopupOpenBtn.addEventListener('click', openCardAddPopup);
+locationPopupOpenBtn.addEventListener('click', function () {
+  openPopup(cardAddPopup);
+});
 
-cardAddPopupCloseBtn.addEventListener('click', cardAddPopupClose);
+cardAddPopupCloseBtn.addEventListener('click', function () {
+  closePopup(cardAddPopup);
+});
 
 cardAddForm.addEventListener('submit', cardAdd);
 
-imagePopupCloseBtn.addEventListener('click', closeImagePopup);
+imagePopupCloseBtn.addEventListener('click', function () {
+  closePopup(imagePopup);
+});
 
 initCardList();
-
-
-
-
